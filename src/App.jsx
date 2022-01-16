953e19e839982968ce5588d924c476b728985e08
ppwire <poom.mth@gmail.com>
2022-01-16 20:50:10 +0700

    Add Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Button from './components/Button'
 import Content from './components/Content'
+import Contact from './components/Contact'
+
 import MailLogo from './svg/Mail.svg';
 import LinkedInLogo from './svg/Linkedin.svg'
-
 import ProfilePicture from './images/me.jpg'
 
 function App() {
@@ -21,10 +22,11 @@ function App() {
             <Button name="Linkedin" color="button-blue" logo={LinkedInLogo}></Button>
           </div>
           <section>
-            <Content title="About" description="I'm poom a full-stack javascript developer."></Content>
+            <Content title="About" description="I'm poom, a full-stack javascript developer."></Content>
             <Content title="Interests" description="Sexy stuff. Ninja. Anime. Comic. ETC."></Content>
           </section>
         </div>
+        <Contact></Contact>
       </Card>
     </div>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,16 @@
-export default function Contract(props){
-   return(
+import Twitter from '../svg/Twitter_Icon.svg'
+import Facebook from '../svg/Facebook_Icon.svg'
+import Instagram from '../svg/Instagram _Icon.svg'
+import LinkedIn from '../svg/Linkedin_Icon.svg'
+import GitHub from '../svg/GitHub_Icon.svg'
+export default function Contract(props) {
+   return (
       <div className="card-footer">
-         
+         <img src={Twitter} alt="" />
+         <img src={Facebook} alt="" />
+         <img src={Instagram} alt="" />
+         <img src={LinkedIn} alt="" />
+         <img src={GitHub} alt="" />
       </div>
    )
 }
